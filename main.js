6d3a3b84b059d52c94f6ea4445674b94094f939e
qchengq <110155344+qchengq@users.noreply.github.com>
2022-11-30 19:41:45 +0800

    Update main.js

--- main.js
+++ main.js
@@ -3,23 +3,24 @@ console.clear();
 http.__okhttp__.setTimeout(10000);
 
 let url = [
-    'https://ghproxy.com/https://raw.githubusercontent.com/qchengq/laosijishenqi/main/UI.js',
-    'https://raw.githubusercontent.com/qchengq/laosijishenqi/main/UI.js',
-    'https://cdn.jsdelivr.net/gh/qchengq/laosijishenqi@main/UI.js',
-    'https://raw.gh.fakev.cn/qchengq/laosijishenqi/main/UI.js',
+  'https://ghproxy.com/https://raw.githubusercontent.com/qchengq/laosijishenqi/main/ziyong/UI1.js',
+  "https://raw.gh.fakev.cn/qchengq/laosijishenqi/main/ziyong/UI1.js",
+  'https://cdn.jsdelivr.net/gh/qchengq/laosijishenqi@main/ziyong/UI1.js',
+  'https://raw.githubusercontent.com/qchengq/laosijishenqi/main/ziyong/UI1.js',
 ];
 
 for (var i = 0; i < url.length; i++) {
-    try {
-        let res = http.get(url[i]);
-        console.log(i + ":" + res.statusCode);
-        if (res.statusCode == 200) {
-            var UI = res.body.string();
-            if (UI.indexOf('"ui";') == 0) break;
-        } else {
-            toastLog('UI脚本:地址' + i + '下载失败');
-        }
-    } catch (error) {}
+  try {
+      let res = http.get(url[i]);
+      console.log(i + ":" + res.statusCode);
+      if (res.statusCode == 200) {
+          var UI = res.body.string();
+          if (UI.indexOf('"ui";') == 0) break;
+      } else {
+          toastLog('UI脚本:地址' + i + '下载失败');
+      }
+  } catch (error) {}
 }
 
 engines.execScript("UI", UI);
+
